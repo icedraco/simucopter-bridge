3a1c18200b4f8735aa3ae674e283e2d07e840d07
IceDragon <icedragon@quickfox.org>
2017-06-19 08:56:35 +0300

    minor adjustments

--- simucopter_test/TestUnitBridgeClient.cpp
+++ simucopter_test/TestUnitBridgeClient.cpp
@@ -2,6 +2,7 @@
 // Unit Test Case for BridgeClient component
 //
 
+#include <thread>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -18,30 +19,87 @@ namespace {
     class TestUnitBridgeClient: public testing::Test {
     public:
         BridgeMessage msg;
-        BridgeClient client;
-
         TestUnitBridgeClient() :
                 msg(BridgeMessageType::REQUEST, COMMON_MSG_ID)
         {
         }
     };
+
+    void command_receiver(zmq::socket_t* ssub, int* counter) {
+        printf("Thread started!\n");
+        *counter = 0;
+        SIMUCOPTER::ZmqBridgeMessageSerializer serializer;
+        bool depleted = false;
+        while (*counter == 0 || !depleted) {
+            zmq::message_t msg;
+            depleted = !ssub->recv(&msg, ZMQ_NOBLOCK);
+            if (!depleted) {
+                BridgeMessage bm = serializer.deserialize(msg);
+//                printf(">> Caught a message! (%d) id:%d type:%d\n", *counter, bm.id, bm.type);
+                if (bm.id == COMMON_MSG_ID) {
+                    (*counter)++;
+//                    printf("!! Found a BridgeMessage :D\n");
+                }
+            }
+        }
+    }
 }
 
 
 TEST_F(TestUnitBridgeClient, NotInitializedByDefault) {
+    BridgeClient client;
     ASSERT_FALSE(client.is_initialized());
 }
 
 TEST_F(TestUnitBridgeClient, InitSetsInitializedFlag) {
+    BridgeClient client;
     ASSERT_FALSE(client.is_initialized()) << "Pre-condition failed: BridgeClient is already initialized!";
     client.init();
     ASSERT_TRUE(client.is_initialized());
+    client.close();
+}
+
+TEST_F(TestUnitBridgeClient, CloseUnsetsInitializedFlag) {
+    BridgeClient client;
+    ASSERT_FALSE(client.is_initialized()) << "Pre-condition failed: BridgeClient is already initialized!";
+    client.init();
+    ASSERT_TRUE(client.is_initialized());
+    client.close();
+    ASSERT_FALSE(client.is_initialized());
 }
 
 TEST_F(TestUnitBridgeClient, CommandWithoutServiceTimesOut) {
+    BridgeClient client;
     client.init();
     client.send_command(COMMON_MSG_ID);
     client.send_command(COMMON_MSG_ID, 0.01);
     client.send_command(COMMON_MSG_ID, 0.01, 0.02);
+    client.close();
     ASSERT_TRUE(true);
+}
+
+TEST_F(TestUnitBridgeClient, SendCommandArrivesAtDestination) {
+    std::string cmd_url = "tcp://127.0.0.1:31337";
+
+    zmq::context_t& ctx = SIMUCOPTER::ZmqContextContainer::get_context();
+    zmq::socket_t ssub(ctx, ZMQ_SUB);
+    ssub.bind(cmd_url);
+    ssub.setsockopt(ZMQ_RCVTIMEO, -1);
+    ssub.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+
+    BridgeClient client(SIMUCOPTER::ZMQ_BRIDGE_REQ_URL, cmd_url);
+    client.init();
+
+    int counter = 0;
+    std::thread t(command_receiver, &ssub, &counter);
+
+    usleep(250000);
+    client.send_command(COMMON_MSG_ID);
+    client.send_command(COMMON_MSG_ID);
+    client.send_command(COMMON_MSG_ID);
+
+    t.join();
+    ssub.close();
+    client.close();
+    ASSERT_GT(counter, 0);
 }
--- simucopter_test/TestUnitBridgeService.cpp
+++ simucopter_test/TestUnitBridgeService.cpp
@@ -2,10 +2,12 @@
 // Unit Test Case for BridgeService component
 //
 
+#include <cstdlib>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
 #include "BridgeService.h"
+#include "test-common.h"
 
 using testing::Eq;
 using SIMUCOPTER::BridgeService;
@@ -13,9 +15,10 @@ using SIMUCOPTER::BridgeMessage;
 using SIMUCOPTER::BridgeMessageType;
 
 namespace {
+    static const int USEC_SLEEP_AFTER_CLOSE = 1000;
     static const int COMMON_MSG_ID = 1234;
 
-    class DummyRequestHandler: public SIMUCOPTER::BridgeRequestHandler {
+    class DummyRequestHandler: public SIMUCOPTER::AbstractBridgeRequestHandler {
         void handle(const BridgeMessage& msg, BridgeMessage& response) {
             std::stringstream ss_response;
             ss_response << "OK type#" << msg.type << " id#" << msg.id;
@@ -26,45 +29,73 @@ namespace {
 
     class TestUnitBridgeService: public testing::Test {
     public:
+        std::string current_req_url;
+        std::string current_cmd_url;
         BridgeMessage msg;
-        BridgeService service;
-        DummyRequestHandler handler;
-
         TestUnitBridgeService() :
                 msg(BridgeMessageType::REQUEST, COMMON_MSG_ID)
         {
-            service.set_request_handler(COMMON_MSG_ID, &handler);
+        }
+
+    protected:
+        virtual void SetUp() {
+            current_req_url = get_random_zmq_url();
+            current_cmd_url = get_random_zmq_url();
         }
     };
 }
 
 
 TEST_F(TestUnitBridgeService, NotInitializedByDefault) {
+    BridgeService service(current_req_url, current_cmd_url);
     ASSERT_FALSE(service.is_initialized());
 }
 
 TEST_F(TestUnitBridgeService, InitSetsInitializedFlag) {
+    BridgeService service(current_req_url, current_cmd_url);
     ASSERT_FALSE(service.is_initialized()) << "Pre-condition failed: BridgeService is already initialized!";
     service.init();
     ASSERT_TRUE(service.is_initialized());
+    service.close();
+    usleep(USEC_SLEEP_AFTER_CLOSE);
+}
+
+TEST_F(TestUnitBridgeService, CloseUnsetsInitializedFlag) {
+    BridgeService service(current_req_url, current_cmd_url);
+    service.init();
+    ASSERT_TRUE(service.is_initialized());
+    service.close();
+    usleep(USEC_SLEEP_AFTER_CLOSE);
+    ASSERT_FALSE(service.is_initialized());
 }
 
 TEST_F(TestUnitBridgeService, HandlerAssignmentWorks) {
+    BridgeService service(current_req_url, current_cmd_url);
+    DummyRequestHandler handler;
+
+    service.set_request_handler(COMMON_MSG_ID, &handler);
     ASSERT_EQ(&handler, &service.handler(COMMON_MSG_ID));
 }
 
 TEST_F(TestUnitBridgeService, UnassignedHandlerLookupReturnsZeroRequestHandler) {
+    BridgeService service(current_req_url, current_cmd_url);
     ASSERT_EQ(service.default_handler(), &service.handler(0));
 }
 
 TEST_F(TestUnitBridgeService, ClearRequestHandlerWorks) {
+    BridgeService service(current_req_url, current_cmd_url);
+    DummyRequestHandler handler;
+
+    service.set_request_handler(COMMON_MSG_ID, &handler);
     ASSERT_EQ(&handler, &service.handler(COMMON_MSG_ID));
     service.clear_request_handler(COMMON_MSG_ID);
     ASSERT_EQ(service.default_handler(), &service.handler(COMMON_MSG_ID));
 }
 
-TEST_F(TestUnitBridgeService, UpdateCallWithoutMessagesDoesntBlock) {
-    service.init();
-    service.update();
-    ASSERT_TRUE(true);
-}
+//TEST_F(TestUnitBridgeService, UpdateCallWithoutMessagesDoesntBlock) {
+//    BridgeService service(current_req_url, current_cmd_url);
+//    service.init();
+//    service.update();
+//    service.close();
+//    ASSERT_TRUE(true);
+//}
