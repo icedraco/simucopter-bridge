7aaba71c16f67286402539a5e000f7611fb1337b
IceDragon <icedragon@quickfox.org>
2017-06-25 23:00:58 +0300

    prevent ardupilot request thread from going out of scope and dying

--- simucopter/simucopter-arducopter.cpp
+++ simucopter/simucopter-arducopter.cpp
@@ -4,6 +4,7 @@
 namespace SIMUCOPTER {
     BridgeService            simucopter_bridge_service("tcp://127.0.0.1:5555", "tcp://127.0.0.1:5556");
     ArduCopterRequestHandler simucopter_request_handler;
+	std::thread*             simucopter_request_thread;
 
     void request_thread(void) {
 	    simucopter_bridge_service.init();
@@ -15,7 +16,7 @@ namespace SIMUCOPTER {
 	}
 
 	void init_simucopter(void) {
-	    std::thread t(request_thread);
+        simucopter_request_thread = new std::thread(request_thread);
 	}
 }
 
