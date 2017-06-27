d17c61b4a5e78c3599bf9e83cc03c43f4ae50112
IceDragon <icedragon@quickfox.org>
2017-06-27 10:35:00 +0300

    include fixes + SITL function declarations

--- simucopter/simucopter.h
+++ simucopter/simucopter.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #ifdef __cplusplus
-#include <BridgeClient.h>
-#include <simucopter-bridge.h>
 
 #include "SimulinkBridgeInterface.h"
 #include "SitlBridgeInterface.h"
+
 
 extern "C" {
 #endif
@@ -15,9 +14,10 @@ void simucopter_stop();
 
 
 /*****************************************************************************\
- * Flight Mode Functions *****************************************************
+ * SimuCopter Block Functions ************************************************
 \*****************************************************************************/
 
+//--- ArduCopter ------------------------------------------------------------//
 double copter_get_accel_x();
 double copter_get_accel_y();
 double copter_get_accel_z();
@@ -39,7 +39,20 @@ void copter_motors_set_pitch(double pitch);
 void copter_motors_set_yaw(double yaw);
 void copter_motors_set_throttle(double throttle);
 void copter_gcs_send_text(int severity, const char* str);
-double sitl_whatever();
+
+
+//--- SITL ------------------------------------------------------------------//
+double sitl_get_state_yaw();
+double sitl_get_state_roll();
+double sitl_get_state_pitch();
+double sitl_get_accel_x();
+double sitl_get_accel_y();
+double sitl_get_accel_z();
+double sitl_get_gyro_x();
+double sitl_get_gyro_y();
+double sitl_get_gyro_z();
+double sitl_get_height();
+double sitl_get_heading();
 
 #ifdef __cplusplus
 };
